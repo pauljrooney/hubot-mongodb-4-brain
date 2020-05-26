e6b9a309db9d081bcf9188249cb137951f7f6267
Paul Rooney <paul.rooney@gmail.com>
2020-05-26 16:44:00 -0700

    undo of es module definition

--- scripts/hubot-mongodb-4-brain.js
+++ scripts/hubot-mongodb-4-brain.js
@@ -1,55 +1,48 @@
-let mdb_client = require('mongodb').MongoClient;
-
 module.exports = (robot) => {
     const MONGODB_URL = process.env.MONGODB_URL;
+    const client = require('mongodb').MongoClient(MONGODB_URL,{useUnifiedTopology:true});
+    const brainCollection = client.db().collection("brain");
 
-    mdb_client.connect(MONGODB_URL, (err, client) => {
-        let db = client.db("tsbot");
+    robot.logger.info("Connected to MongoDB.");
+    robot.brain.on("close", () => {
+        client.close();
+    });
+    robot.brain.setAutoSave(false);
 
-        robot.logger.info("Connected to MongoDB.");
-        robot.brain.on("close", () => {
-            client.close();
-        });
-        robot.brain.setAutoSave(false);
+    let cache = {};
 
-        let cache = {};
 
-        db.createCollection("brain", (err, collection) => {
-            collection.find({}).toArray((err, docs) => {
+    brainCollection.find({}).toArray((err, docs) => {
+        if(err) {
+            robot.logger.error(err);
+        }
+        data = {};
+
+        for(doc in docs) {
+            let docContent = doc.content;
+            data[doc._id] = docContent;
+            cache[doc._id] = docContent;
+        }
+
+        robot.brain.mergeData(data);
+        robot.brain.resetSaveInterval(10);
+        robot.brain.setAutoSave(true);
+    });
+
+    robot.brain.on("save", (data) => {
+        for(const [key, value] of Object.entries(data)) {
+            if(cache[key] === value) {
+                return;
+            }
+
+            robot.logger.info(`Saving ${key} into brain...`);
+            cache[key] = value;
+            brainCollection.updateOne({_id:key},{$set:{content:value}}, {upsert: true}, (err, res) => {
                 if(err) {
                     robot.logger.error(err);
                 }
-                data = {};
-
-                for(doc in docs) {
-                    let docContent = doc.content;
-                    data[doc._id] = docContent;
-                    cache[doc._id] = docContent;
-                }
-
-                robot.brain.mergeData(data);
-                robot.brain.resetSaveInterval(10);
-                robot.brain.setAutoSave(true);
             });
-        });
-
-        robot.brain.on("save", (data) => {
-            db.collection("brain", (err, collection) => {
-                for(const [key, value] of Object.entries(data)) {
-                    if(cache[key] === value) {
-                        return;
-                    }
-
-                    robot.logger.info(`Saving ${key} into brain...`);
-                    cache[key] = value;
-                    collection.updateOne({_id:key},{$set:{content:value}}, {upsert: true}, (err, res) => {
-                        if(err) {
-                            robot.logger.error(err);
-                        }
-                    });
-                    return;
-                }
-            });
-        });
+            return;
+        }
     });
 }
